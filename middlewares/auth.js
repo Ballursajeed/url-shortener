36883b6983f469844d28faf1a36a9e4794550d7f
Ballursajeed <ballursajeed@gmail.com>
2023-12-09 20:31:08 +0530

    added some files

---
+++ middlewares/auth.js
@@ -0,0 +1,35 @@
+const { getUser } = require("../service/auth");
+
+ function checkForAuthentication(req,res,next) {
+     const tokenCookie = req.cookies?.token;
+     req.user = null;
+     if(!tokenCookie) {
+        return next();
+     }
+     const token = tokenCookie;
+     const user = getUser(token);
+
+     req.user = user;
+     return next();
+ }
+
+ //Autherization
+function restrictTo(roles = []) {
+  return function(req,res,next) {
+    if(!req.user) {
+       return res.redirect("/login");
+    }
+    if(!roles.includes(req.user.role)) {
+    	console.log("req.user",req.user);
+    	console.log("req.user.role:",req.user.role);
+         return res.end("UnAuthorized");
+        }
+    return next();
+  }
+}
+
+
+module.exports = {
+ checkForAuthentication,
+ restrictTo,
+};
